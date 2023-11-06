9d5a3cc01f26dbce3ac6788c4bb460c4be7f2b04
Juany Punte <juanigpunte@gmail.com>
2023-11-06 17:30:20 -0300

    apidolar finalizada, retoques esteticos. Falta: funciones carrito, archivo JSON

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ const motherboards = [
     {
     id: "mother1",
     nombre: 'Gigabyte B450M ds3h v2',
-    price: 90000,
+    price: 153,
     thumbnail: 'images/Mother_Gigabyte_B450M_DS3H_V2_DDR4_AM4_22c8d45a-grn.jpg',
     
 },
 {
     id: "mother2",
     nombre: 'MSI H510M PRO-E',
-    price: 120000,
+    price: 165,
     thumbnail:'images/Mother_MSI_H510M_PRO-E_DDR4_LGA_1200_89a861ba-grn.jpg',
 },
 {
     id: "mother3",
     nombre: 'MSI X670-P PRO WIFI',
-    price: 200000,
+    price: 190,
     thumbnail:'images/Mother_MSI_X670-P_PRO_WIFI_AM5_be20ca6c-grn.jpg',
 }
 ];
@@ -24,21 +24,21 @@ const cpus = [
     {
     id: "cpu1",
     nombre: 'Ryzen 5 5600x',
-    price: 200000,
+    price: 252,
     thumbnail: 'images/AMD_Ryzen_5_5600X_4.6GHz_Turbo_AM4___Wraith_Stealth_Cooler_f737ec9f-grn.jpg',
     
 },
 {
     id: "cpu2",
     nombre: 'Ryzen 7 5800x',
-    price: 250000,
+    price: 290,
     thumbnail: 'images/Procesador_AMD_Ryzen_7_7700X_5.4GHz_Turbo_AM5_-_No_incluye_Cooler_-_C_VIDEO_5db10196-grn.jpg',
     
 },
 {
     id: "cpu3",
     nombre: 'Intel i7 11700KF',
-    price: 220000,
+    price: 350,
     thumbnail: 'images/Intel_Core_i7_11700KF_5.0GHz_Turbo_Socket_1200_Rocket_Lake_56a482fd-grn.jpg',
     
 }
@@ -48,19 +48,19 @@ const gpus = [
     {
     id: "gpu1",
     nombre: 'RTX 3060 MSI VENTUS',
-    price: 447000,
+    price: 450,
     
 },
 {
     id: "gpu2",
     nombre: 'RTX 4060ti',
-    price: 538000,
+    price: 680,
     
 },
 {
     id: "gpu3",
     nombre: 'RX 5600 xt',
-    price: 350000,
+    price: 600,
     
 }
 ];
@@ -69,19 +69,19 @@ const psu = [
     {
     id: "psu1",
     nombre: 'Fuente Be Quiet! 600W 80 Plus Bronze U9',
-    price: 60000,
+    price: 70,
     
 },
 {
     id: "psu2",
     nombre: 'Fuente ASUS ROG 1000W 80 Plus Gold',
-    price: 80000,
+    price: 170,
     
 },
 {
     id: "psu3",
     nombre: 'Fuente Gigabyte 450W 80 Plus Bronze P450B',
-    price: 56000,
+    price: 60,
     
 }
 ];
@@ -126,7 +126,7 @@ const Card = (props) => {
       <img src= ${thumbnail} alt="foto de ${nombre}"></img>
      <div class="card-body">
       <h2 id="name">${nombre}</h2>
-      <p id="price">ARS: ${price}</p>
+      <p id="price">U$D: ${price}</p>
       <button onclick="agregarAlcarrito('${id}')" id="save-button" type="button" class="button">
     <span class="button__text">Comprar</span>
     <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
@@ -200,7 +200,7 @@ const carrito = [];
                 <img src= ${product.thumbnail} alt="foto de ${product.nombre}"></img>
                 <div class="card-body">
                     <h2 id="name">${product.nombre}</h2>
-                    <p id="price">ARS: ${product.price}</p>
+                    <p id="price">USD: ${product.price}</p>
                     <a class="borrar-btn" id="${product.id}"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                   </svg></a>
@@ -227,4 +227,8 @@ mostrarProductos();
 
 fetch("https://dolarapi.com/v1/dolares/blue")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    let dolarBlue = data.venta;
+    dolarContainer = document.querySelector('#usd')
+    dolarContainer.innerHTML = dolarBlue;
+  } );
